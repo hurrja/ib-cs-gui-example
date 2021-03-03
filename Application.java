68d32d50738f7e1b619b117171b305c4b9c60962
Jarmo Hurri <jarmo.hurri@iki.fi>
2021-03-03 08:24:30 +0200

    * use lambda expression in invocation in main()

--- Application.java
+++ Application.java
@@ -13,9 +13,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-public class Application implements Runnable
+public class Application
 {
-  public void run ()
+  public void initialize ()
   {
     new GUI (this);
     state = false;
--- Main.java
+++ Main.java
@@ -20,7 +20,6 @@ public class Main
   public static void main (String[] args)
   {
     Application application = new Application ();
-    SwingUtilities.invokeLater (application);
+    SwingUtilities.invokeLater (() -> application.initialize ());
   }
 }
-
