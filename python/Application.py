a9250451ccc99ff70ab592eccc6ac476d31bd73d
Jarmo Hurri <jarmo.hurri@iki.fi>
2025-04-15 13:16:32 +0300

    * python pyside6 version of the app

---
+++ python/Application.py
@@ -0,0 +1,27 @@
+from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QTimer
+
+import GUI
+import sys
+
+class Application:
+
+    def run (self):
+        self.state = False # "state" of app is one boolean variable
+        self.gui = GUI.GUI (self)
+
+        # timer running every second; needs to be an attribute so that
+        # the timer is not garbage collected
+        self.timer = QTimer ()
+        self.timer.setInterval (1000)
+        self.timer.timeout.connect (self.gui.update_and_show_time)
+        self.timer.start ()
+
+    def quit (self):
+        sys.exit ()
+
+    def flip_state (self):
+        self.state = not self.state
+
+    def get_state (self):
+        return self.state
---
+++ python/main.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QTimer
+import sys
+from Application import Application
+
+application = Application ()
+QTimer.singleShot (0, application.run)
+QApplication (sys.argv).exec ()
