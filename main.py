94d0d2bdeadae5db28c604d6a2978ca0a97590f5
Jarmo Hurri <jarmo.hurri@iki.fi>
2025-10-26 11:02:32 +0200

    * larger font

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from PySide6.QtCore import QTimer
 import sys
 from Application import Application
 
+q_app = QApplication(sys.argv)
+# larger font size for better visibility
+font = q_app.font()
+font.setPointSize(2 * font.pointSize())
+q_app.setFont(font)
+
+# eval run() of class Application when QApplication runs
 application = Application()
 QTimer.singleShot(0, application.run)
-QApplication(sys.argv).exec()
+q_app.exec()
